e0ed17370405f644fecde2baccfdd0f230739709
Alexander Hsu <hsu.alex66@gmail.com>
2024-03-22 03:37:08 -0400

    testing

--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -9,23 +9,23 @@ import java.io.File;
 import Part_1.Method;
 
 public class Driver {
-    
+
     public static void do_part1(String part1_manifest) {
-       
+
         File folder = new File(part1_manifest);
         File[] listOftextFiles = folder.listFiles();
 
-        if(listOftextFiles != null){
-            for(File file: listOftextFiles){
-                if(file.isFile() && file.getName().endsWith(".csv")){
-                    try{
+        if (listOftextFiles != null) {
+            for (File file : listOftextFiles) {
+                if (file.isFile() && file.getName().endsWith(".csv")) {
+                    try {
                         BufferedReader br = new BufferedReader(new FileReader(file));
-                        
+
                         FileWriter badRecordsWriter = new FileWriter("bad_movie_records.txt");
                         FileWriter part2_manifestWriter = new FileWriter("part2_manifest.txt");
-                        
+
                         String line = "";
-                        while((line = br.readLine()) != null){
+                        while ((line = br.readLine()) != null) {
                             String inputFileName = line.trim();
                             Method.partitionMovies(inputFileName, badRecordsWriter, part2_manifestWriter);
                         }
@@ -33,14 +33,14 @@ public class Driver {
                         br.close();
                         badRecordsWriter.close();
                         part2_manifestWriter.close();
-                        
-                    }catch(IOException e){
+
+                    } catch (IOException e) {
                         System.out.println("Error reading file: " + file.getName());
                         e.printStackTrace();
                     }
                 }
             }
-        }else{
+        } else {
             System.out.println("No files found in the directory");
         }
 
@@ -58,9 +58,13 @@ public class Driver {
         String part1_manifest = "DataBase/";
 
         do_part1(part1_manifest);
-        /*  String part2_manifest = do_part1(part1_manifest);
-        String part3_manifest = do_part2(part2_manifest);
-        do_part3(part3_manifest);
-        return;*/
+        /*
+         * String part2_manifest = do_part1(part1_manifest);
+         * String part3_manifest = do_part2(part2_manifest);
+         * do_part3(part3_manifest);
+         * return;
+         */
+
+        /* testing */
     }
 }
