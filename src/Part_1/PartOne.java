9ed7a6ad11c8389bc32e67a5a7efc9f543099f68
Alexander Hsu <hsu.alex66@gmail.com>
2024-03-25 03:14:02 -0400

    doPart1() is able to read the csv files and they are stored into a badMovieArray and goodMovieArray

--- src/Part_1/PartOne.java
+++ src/Part_1/PartOne.java
@@ -19,78 +19,118 @@ import Exceptions.MissingQuotesException;
 import Exceptions.BadNameException;
 
 public class PartOne {
-    
+
     /*
      * An array to store file names for further processing
      */
 
-     public static String[] fileNames;
+    public static String[] fileNames;
 
-     public static void doPart1(){
+    public static void doPart1() {
         // String manifest = "part1_manifest.txt";
         // java.io.File maninfestFile = new java.io.File("src/Driver/"+manifest);
-        
-        // // todo : for loop, that reads the manifest file and reads the files in the manifest file
 
-
-
-
-
+        // // todo : for loop, that reads the manifest file and reads the files in the
+        // manifest file
 
         BufferedReader reader = null;
+        String line = "";
 
-        java.io.File directoryName = new java.io.File("src/DataBase/");
+        File directoryName = new File("src/DataBase/");
 
-        try{
-            //File folder = new File("src/Driver/part1_manifest.txt");
+        try {
+            // File folder = new File("src/Driver/part1_manifest.txt");
+            int row = 0;
             for (int i = 0; i < directoryName.list().length; i++) {
-                String count =  String.valueOf(i);
-                final File folder = new File("src/DataBase/Movies199"+count+".csv");
+                String count = String.valueOf(i);
+                File folder = new File("src/DataBase/Movies199" + count + ".csv");
+
                 reader = new BufferedReader(new FileReader(folder));
-                int sizeLine = Integer.parseInt(reader.readLine());
-                fileNames = new String[sizeLine];
-                
-                for(int z = 0; z < sizeLine; z++){
-                    fileNames[z] = reader.readLine();
+
+                while ((line = reader.readLine()) != null) {
+                    // String[] values = line.split(",");
+
+                    if (line.split("") != null) {
+                        row++;
+                        System.out.println(line);
+                    }
                 }
-                
+            }
+            System.out.println("THE ROW IS : " + row);
+            String[] badMovie = new String[row];
+            String[] movieArray = new String[row];
+
+            int goodMovieIndex = 0;
+            int badMovieIndex = 0;
+            for (int i = 0; i < directoryName.list().length; i++) {
+                String count = String.valueOf(i);
+                System.out.println(directoryName.list().length);
+                File folder = new File("src/DataBase/Movies199" + count + ".csv");
+
+                reader = new BufferedReader(new FileReader(folder));
+
+                while ((line = reader.readLine()) != null) {
+                    String[] values = line.split(",");
+
+                    if (line.split("") != null) {
+
+                        if (values.length > 10) {
+                            System.out.println("Hello ");
+                        }
+                        if (values.length == 10) {
+                            for (int j = 0; j < 10; j++) {
+                                movieArray[goodMovieIndex] = line;
+                            }
+                            goodMovieIndex++;
+                            System.out.println(line);
+                        } else {
+                            badMovie[badMovieIndex] = line;
+                            badMovieIndex++;
+
+                        }
+                    }
+                }
             }
 
             reader.close();
 
-            for(String fileName: fileNames){
-                
-                File inputFile = new File(fileName);
+            // sorting the bad movies into it's file
+            // for (String fileName : fileNames) {
 
-                if(inputFile.canRead()){
-                    
-                    BufferedReader fileReader = new BufferedReader(new FileReader(inputFile));
-                    String line;
+            // File inputFile = new File(fileName);
 
-                    while((line = fileReader.readLine()) != null){
-                        validateAndWriteRecord(line, fileName);
-                    }
-                    fileReader.close();
-                }
-            }
-        }catch(Exception e){
+            // if (inputFile.canRead()) {
+
+            // BufferedReader fileReader = new BufferedReader(new FileReader(inputFile));
+
+            // while ((line = fileReader.readLine()) != null) {
+            // System.out.println("Program is validating and writing to record"); // testing
+            // validateAndWriteRecord(line, fileName);
+            // }
+            // fileReader.close();
+            // }
+            // }
+        } catch (Exception e) {
             System.out.println("Error reading file: " + e.getMessage());
+            e.printStackTrace(); // testing
         }
 
-     }
+    }
 
-     public static void validateAndWriteRecord(String line, String fileName) throws IOException, BadGenreException, MissingFieldsException, ExcessFieldsException, MissingQuotesException, BadYearException, BadTitleException, BadScoreException, BadDurationException, BadRatingException, BadNameException{
-        
+    public static void validateAndWriteRecord(String line, String fileName) throws IOException, BadGenreException,
+            MissingFieldsException, ExcessFieldsException, MissingQuotesException, BadYearException, BadTitleException,
+            BadScoreException, BadDurationException, BadRatingException, BadNameException {
+
         String genre = "";
         String[] movieRecords = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
 
-        try{
+        try {
 
-            if(movieRecords.length == 10){
+            if (movieRecords.length == 10) {
 
                 genre = findGenre(movieRecords[3]);
 
-                if(!isValidGenre(genre)){
+                if (!isValidGenre(genre)) {
 
                     throw new BadGenreException("Semantic Error: " + genre + " is not a valid genre");
 
@@ -99,50 +139,51 @@ public class PartOne {
                 String writeToFile = OutputFile(genre);
                 writeRecordToFile(writeToFile, line);
 
-            }else if(movieRecords.length < 10){
+            } else if (movieRecords.length < 10) {
 
                 throw new MissingFieldsException("Syntax Error: Missing fields in record: " + line);
 
-            }else if(movieRecords.length > 10){
+            } else if (movieRecords.length > 10) {
 
                 throw new ExcessFieldsException("Syntax Error: Excess fields in record: " + line);
 
-            }else if(!movieRecords[1].startsWith("\"") || !movieRecords[1].endsWith("\"")){
+            } else if (!movieRecords[1].startsWith("\"") || !movieRecords[1].endsWith("\"")) {
 
                 throw new MissingQuotesException("Syntax Error: Missing quotes in record: " + line);
 
-            }else if(Integer.parseInt(movieRecords[0]) < 1990 || Integer.parseInt(movieRecords[0]) > 1999){
+            } else if (Integer.parseInt(movieRecords[0]) < 1990 || Integer.parseInt(movieRecords[0]) > 1999) {
 
                 throw new BadYearException("Semantic Error: Year is not between 1990 and 1999 in record: " + line);
 
-            }else if(Integer.parseInt(movieRecords[2]) < 0 || Integer.parseInt(movieRecords[2]) > 300){
+            } else if (Integer.parseInt(movieRecords[2]) < 0 || Integer.parseInt(movieRecords[2]) > 300) {
 
                 throw new BadDurationException("Semantic Error: Duration is not between 0 and 300 in record: " + line);
 
-            }else if(Double.parseDouble(movieRecords[5]) < 0 || Double.parseDouble(movieRecords[5]) > 10){
+            } else if (Double.parseDouble(movieRecords[5]) < 0 || Double.parseDouble(movieRecords[5]) > 10) {
 
                 throw new BadScoreException("Semantic Error: Score is not between 0 and 10 in record: " + line);
 
-            }else if(!isValidRating(movieRecords[4])){
+            } else if (!isValidRating(movieRecords[4])) {
 
                 throw new BadRatingException("Semantic Error: Rating is not valid in record: " + line);
 
-            }else if(movieRecords[1].split("\"").length > 3){
+            } else if (movieRecords[1].split("\"").length > 3) {
 
                 throw new BadTitleException("Semantic Error: Excess fields in record: " + line);
 
-            }else{
+            } else {
 
                 throw new BadNameException("Semantic Error: Name is not valid in record: " + line);
 
             }
 
-        }catch(BadYearException | BadTitleException | BadScoreException | BadDurationException | BadRatingException | BadNameException e){
-           
+        } catch (BadYearException | BadTitleException | BadScoreException | BadDurationException | BadRatingException
+                | BadNameException e) {
+
             String writeOutputToFile = "bad_movie_records.txt";
             FileWriter writer = new FileWriter(writeOutputToFile, true);
             BufferedWriter writeToBadFile = new BufferedWriter(writer);
-           
+
             writeToBadFile.write("\n------------------------\n");
             writeToBadFile.write("Semantic Error:" + fileName);
             writeToBadFile.write("\n------------------------\n");
@@ -158,19 +199,19 @@ public class PartOne {
         return rating.equals("G") || rating.equals("PG") || rating.equals("PG-13") ||
                 rating.equals("R") || rating.equals("NC-17") || rating.equals("Unrated");
     }
-    
-    public static String findGenre(String movieRecords){
-        if(movieRecords.startsWith("\"") && movieRecords.endsWith("\"")){
+
+    public static String findGenre(String movieRecords) {
+        if (movieRecords.startsWith("\"") && movieRecords.endsWith("\"")) {
             return movieRecords.substring(1, movieRecords.length() - 1);
-        }else{
+        } else {
             return movieRecords;
         }
     }
 
     private static boolean isValidGenre(String genre) {
-        String[] validGenres = {"musical", "comedy", "animation", "adventure", "drama", "crime",
+        String[] validGenres = { "musical", "comedy", "animation", "adventure", "drama", "crime",
                 "biography", "horror", "action", "documentary", "fantasy", "mystery", "sci-fi",
-                "family", "romance", "thriller", "western"};
+                "family", "romance", "thriller", "western" };
         for (String validGenre : validGenres) {
             if (validGenre.equals(validGenre)) {
                 return true;
@@ -179,11 +220,11 @@ public class PartOne {
         return false;
     }
 
-    public static String OutputFile(String genre){
-        
+    public static String OutputFile(String genre) {
+
         String outputFile;
 
-        switch(genre){
+        switch (genre) {
             case "musical":
                 outputFile = "musical.txt";
                 break;
@@ -242,8 +283,8 @@ public class PartOne {
         return outputFile;
     }
 
-    public static void writeRecordToFile(String writeToFile, String record) throws IOException{
-        
+    public static void writeRecordToFile(String writeToFile, String record) throws IOException {
+
         try {
             FileWriter writer = new FileWriter(writeToFile, true);
             BufferedWriter writingToFile = new BufferedWriter(writer);
@@ -254,5 +295,3 @@ public class PartOne {
         }
     }
 }
-
-
